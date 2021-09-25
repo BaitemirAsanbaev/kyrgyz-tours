db77ede29d04afae153ed15a9e4c1b977d5d113d
Baitemir Asanbaev <baitemir1706@gmail.com>
2021-09-25 13:30:48 +0600

    swiper pt1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
 import 'bootstrap';
+
+  
+  // import Swiper bundle with all modules installed
+  import Swiper from 'swiper/bundle';
+
+  // import styles bundle
+  import 'swiper/css/bundle';
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 5,
+  centeredSlides: true,
+  spaceBetween: 200,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
